e076c1877995cc3634445ead672bb22b903a1d6b
jeongmin <ljmpretty98@gmail.com>
2018-11-01 20:55:55 +0900

    week3

---
+++ Week3/Week3_HW_jeongmin/MyApplication6/app/src/main/java/com/example/ljmpr/myapplication/FourthFragment.java
@@ -0,0 +1,28 @@
+package com.example.ljmpr.myapplication;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+public class FourthFragment extends Fragment {
+
+    public FourthFragment() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        RelativeLayout layout = (RelativeLayout) inflater.inflate(R.layout.fourth_fragment, container, false);
+        return layout;
+    }
+}
