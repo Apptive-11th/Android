27ad6ce074bcada5eeb838cda837b6a9887b6fa8
jeongmin <ljmpretty98@gmail.com>
2019-01-02 01:43:50 +0900

    수정

--- Week4/Week4_HW_이정민/clone/app/src/main/java/com/example/ljmpr/clone/Friends.java
+++ Week4/Week4_HW_이정민/clone/app/src/main/java/com/example/ljmpr/clone/Friends.java
@@ -3,14 +3,16 @@ package com.example.ljmpr.clone;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
 import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
-public class Friends extends ListFragment {
+public class Friends extends Fragment {
 
     FriendsAdapter adapter;
 
@@ -24,26 +26,30 @@ public class Friends extends ListFragment {
     @Override
     public View onCreateView (@Nullable LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        adapter = new FriendsAdapter();
-        setListAdapter(adapter);
+        LinearLayout layout = (LinearLayout)inflater.inflate(R.layout.fragment_friends,container,false);
 
-        adapter.addItem(R.drawable.baseline_person_black_18dp,"Name1","Sentence1");
-        adapter.addItem(R.drawable.baseline_person_black_18dp,"Name2","Sentence2");
-        adapter.addItem(R.drawable.baseline_person_black_18dp,"Name3","Sentence3");
-        adapter.addItem(R.drawable.baseline_person_black_18dp,"Name4","Sentence4");
-        adapter.addItem(R.drawable.baseline_person_black_18dp,"Name5","Sentence5");
-        adapter.addItem(R.drawable.baseline_person_black_18dp,"Name6","Sentence6");
-        adapter.addItem(R.drawable.baseline_person_black_18dp,"Name7","Sentence7");
-        adapter.addItem(R.drawable.baseline_person_black_18dp,"Name8","Sentence8");
-        adapter.addItem(R.drawable.baseline_person_black_18dp,"Name9","Sentence9");
-        adapter.addItem(R.drawable.baseline_person_black_18dp,"Name10","Sentence10");
-        adapter.addItem(R.drawable.baseline_person_black_18dp,"Name11","Sentence11");
-        adapter.addItem(R.drawable.baseline_person_black_18dp,"Name12","Sentence12");
-        adapter.addItem(R.drawable.baseline_person_black_18dp,"Name13","Sentence13");
-        adapter.addItem(R.drawable.baseline_person_black_18dp,"Name14","Sentence14");
-        adapter.addItem(R.drawable.baseline_person_black_18dp,"Name15","Sentence15");
+        ListView friendsList = (ListView)layout.findViewById(R.id.listView_friends);
+        FriendsAdapter friendsAdapter = new FriendsAdapter();
 
-        return super.onCreateView(inflater, container, savedInstanceState);
+        friendsAdapter.addItem(R.drawable.baseline_person_black_18dp,"Name1","Sentence1");
+        friendsAdapter.addItem(R.drawable.baseline_person_black_18dp,"Name2","Sentence2");
+        friendsAdapter.addItem(R.drawable.baseline_person_black_18dp,"Name3","Sentence3");
+        friendsAdapter.addItem(R.drawable.baseline_person_black_18dp,"Name4","Sentence4");
+        friendsAdapter.addItem(R.drawable.baseline_person_black_18dp,"Name5","Sentence5");
+        friendsAdapter.addItem(R.drawable.baseline_person_black_18dp,"Name6","Sentence6");
+        friendsAdapter.addItem(R.drawable.baseline_person_black_18dp,"Name7","Sentence7");
+        friendsAdapter.addItem(R.drawable.baseline_person_black_18dp,"Name8","Sentence8");
+        friendsAdapter.addItem(R.drawable.baseline_person_black_18dp,"Name9","Sentence9");
+        friendsAdapter.addItem(R.drawable.baseline_person_black_18dp,"Name10","Sentence10");
+        friendsAdapter.addItem(R.drawable.baseline_person_black_18dp,"Name11","Sentence11");
+        friendsAdapter.addItem(R.drawable.baseline_person_black_18dp,"Name12","Sentence12");
+        friendsAdapter.addItem(R.drawable.baseline_person_black_18dp,"Name13","Sentence13");
+        friendsAdapter.addItem(R.drawable.baseline_person_black_18dp,"Name14","Sentence14");
+        friendsAdapter.addItem(R.drawable.baseline_person_black_18dp,"Name15","Sentence15");
+
+        friendsList.setAdapter(friendsAdapter);
+
+        return layout;
     }
 
     public void addItem (int icon, String name, String sen) {
